3e914a0c856ce7713eb812d985cd402c15b6b4da
zhouliang6 <18510971680@163.com>
2018-01-04 19:18:41 +0800

    kotlin

---
+++ app/src/main/java/com/car/shangka/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.car.shangka
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
