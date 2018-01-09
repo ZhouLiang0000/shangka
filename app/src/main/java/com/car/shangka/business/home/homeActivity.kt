45971c74dfb66560f3c64083d222f270afb9d974
zhouliang6 <18510971680@163.com>
2018-01-09 16:28:12 +0800

    kotlin

--- app/src/main/java/com/car/shangka/business/home/homeActivity.kt
+++ app/src/main/java/com/car/shangka/business/home/homeActivity.kt
@@ -1,5 +1,7 @@
 package com.car.shangka.business.home
 
+import android.os.Bundle
+import com.car.shangka.R
 import com.zl.mvp.utils.base.BaseMvpActivity
 
 /**
@@ -9,5 +11,19 @@ import com.zl.mvp.utils.base.BaseMvpActivity
  * 说明：
  */
 class homeActivity : BaseMvpActivity<homeContract.View,homePersenter>(),homeContract.View{
+
     override var mPresenter: homePersenter = homePersenter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    override fun showError(error: String?) {
+        super.showError(error)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
 }
